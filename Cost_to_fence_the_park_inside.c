e99002b7ece5ce0fa101fc0d2489fe5e4647e832
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-02-03 04:02:05 +0530

    #include<stdio.h> #include<math.h> int main() {     int l,b,w,c;     scanf("%d%d%d%d",&l,&b,&w,&c);     int area;     area=(l*b-(l-2*w)*(b-2*w));     if(l<=2*w || b<=2*w)     {         printf("Impossible");     }     else     {         printf("%d",area*c);     } }

--- Cost_to_fence_the_park_inside.c
+++ Cost_to_fence_the_park_inside.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-int main ()
+#include<math.h>
+int main()
 {
-    int l,b,w,c,area,total;
+    int l,b,w,c;
     scanf("%d%d%d%d",&l,&b,&w,&c);
-    if( l<=2*w || b<=2*w)
+    int area;
+    area=(l*b-(l-2*w)*(b-2*w));
+    if(l<=2*w || b<=2*w)
     {
         printf("Impossible");
     }
     else
     {
-    area=l*b-((l-2*w)*(b-2*w));
-    total=area*c;
-    printf("%d",total);
+        printf("%d",area*c);
     }
 }
