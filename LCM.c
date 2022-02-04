81fdabc47f54b0f5587da754f6db08b09848c49e
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-02-04 11:47:44 +0530

    #include<stdio.h> int main() {     int n,m,i=1;     scanf("%d%d",&n,&m);     for(;i;i++)     {         if((m*i)%n==0)         {             printf("%d",m*i);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-   int n1,n2,i=1;
-   scanf("%d%d",&n1,&n2);
-   for(;i;i++)
-   {
-      if((n2*i)%n1==0)
-      {
-          printf("%d",n2*i);
-          break;
-      }
-   }
+    int n,m,i=1;
+    scanf("%d%d",&n,&m);
+    for(;i;i++)
+    {
+        if((m*i)%n==0)
+        {
+            printf("%d",m*i);
+            break;
+        }
+    }
 }
