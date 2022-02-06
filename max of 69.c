9347e5811647ae1ebcfe5fd56f72319f4abfe5cc
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-02-06 19:35:46 +0530

    Add files via upload

---
+++ max of 69.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int number,rev=0,sum=0,count=0;
+    scanf("%d",&number);
+    while(number)
+    {
+        rev=(rev*10)+(number%10);
+        number/=10;
+    }
+    while(rev)
+    {
+        if(rev%10==6 && count==0)
+        {
+            sum=(sum*10)+9;
+            count++;
+        }
+        else
+        {
+            sum=(sum*10)+(rev%10);
+        }
+            rev/=10;
+    }
+    printf("%d",sum);
+}
