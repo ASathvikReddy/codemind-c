db5f2fee081186f0e9b9a431fa257ae6de14f22b
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-05-02 22:31:07 +0530

    #include<stdio.h> int main() {     int n,c;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-2;i++)     {         if(arr[i]%2==0 && arr[i+2]%2==0 && arr[i+1]%2)         {             c++;         }     }     printf("%d",c); }

---
+++ count_odd_numbers_between_even_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,c;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-2;i++)
+    {
+        if(arr[i]%2==0 && arr[i+2]%2==0 && arr[i+1]%2)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
