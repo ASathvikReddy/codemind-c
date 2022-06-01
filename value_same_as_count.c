2a230a941e6a85ec218d6c44e36c5f838efcfce7
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-06-01 20:45:05 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int a[n],c,k;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;                 if(i!=j)                 {                     a[j]=0;                 }             }         }         if(a[i]==c)         {             //printf("%d ",a[i]);             k++;         }     }     printf("%d",k); }

---
+++ value_same_as_count.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int a[n],c,k;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+                if(i!=j)
+                {
+                    a[j]=0;
+                }
+            }
+        }
+        if(a[i]==c)
+        {
+            //printf("%d ",a[i]);
+            k++;
+        }
+    }
+    printf("%d",k);
+}
