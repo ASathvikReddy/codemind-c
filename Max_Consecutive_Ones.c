4bb864698ac5be43cc76fb7d4292825f755d67b7
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-09-28 22:41:12 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],c=0,max=0;     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         if(a[i]==1)         {             c++;         }         if(a[i]==0)         {             if(max<c)             {                 max=c;             }             c=0;         }     }     if(max<c)     {         max=c;     }     printf("%d ",max); }

---
+++ Max_Consecutive_Ones.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],c=0,max=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==1)
+        {
+            c++;
+        }
+        if(a[i]==0)
+        {
+            if(max<c)
+            {
+                max=c;
+            }
+            c=0;
+        }
+    }
+    if(max<c)
+    {
+        max=c;
+    }
+    printf("%d ",max);
+}
