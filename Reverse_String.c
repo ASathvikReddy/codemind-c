0e7538dd211229c290bf90d0ffb26141ce3beb77
ASathvikReddy <97426173+ASathvikReddy@users.noreply.github.com>
2022-11-27 19:48:09 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[1000];     int a[1000],n,i,k=0,j=0,temp;     scanf("%[^ ]s",s);     n=strlen(s);     for(i=0;i<n/2;i++)     {         temp=s[i];         s[i]=s[n-1-i];         s[n-1-i]=temp;     }     for(i=0;s[i];i++)     {         if(s[i]==' ')         {             a[k++]=i;         }     }     a[k]=i;     for(i=0;i<=k;i++)     {         n=a[i]-j;         for(int l=0;l<n/2;l++)         {             temp=s[l+j];             s[l+j]=s[a[i]-1-l];             s[a[i]-1-l]=temp;         }         j=a[i]+1;     }     printf("%s",s); }

--- Reverse_String.c
+++ Reverse_String.c
@@ -1,21 +1,36 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-	char s[100];
-	scanf("%[^
+    char s[1000];
+    int a[1000],n,i,k=0,j=0,temp;
+    scanf("%[^
 ]s",s);
-	int i,j,t;
-	for(i=0;s[i] != NULL;i++) ;
-    int l=i;
-    i=0;
-    j=l-1;
-    while(i<j)
+    n=strlen(s);
+    for(i=0;i<n/2;i++)
     {
- 	 t=s[j];
-   	 s[j]=s[i];
-   	 s[i]=t;
-   	 i++;
-   	 j--;
+        temp=s[i];
+        s[i]=s[n-1-i];
+        s[n-1-i]=temp;
+    }
+    for(i=0;s[i];i++)
+    {
+        if(s[i]==' ')
+        {
+            a[k++]=i;
+        }
+    }
+    a[k]=i;
+    for(i=0;i<=k;i++)
+    {
+        n=a[i]-j;
+        for(int l=0;l<n/2;l++)
+        {
+            temp=s[l+j];
+            s[l+j]=s[a[i]-1-l];
+            s[a[i]-1-l]=temp;
+        }
+        j=a[i]+1;
     }
     printf("%s",s);
 }
